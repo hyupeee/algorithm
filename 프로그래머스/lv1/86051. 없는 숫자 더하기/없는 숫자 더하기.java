bf199e4ef96ec795218c591b1bf4ef5652b4174d
Hyupee <109512458+hyupeee@users.noreply.github.com>
2023-03-20 17:56:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < numbers.length; i ++) {
+            list.add(numbers[i]);
+        }
+        
+        
+        for(int i = 0; i < 10; i++) {
+            if(!list.contains(i)){
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
