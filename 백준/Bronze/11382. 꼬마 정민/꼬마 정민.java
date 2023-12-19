09f19fe54071d32462736fc6e68ecb2dd2f5f4c6
Hyupee <109512458+hyupeee@users.noreply.github.com>
2023-12-19 19:36:53 +0900

    [Bronze V] Title: 꼬마 정민, Time: 228 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Long numA, numB, numC;
+
+        numA = input.nextLong();
+        numB = input.nextLong();
+        numC = input.nextLong();
+
+        System.out.println(numA + numB + numC);
+    }
+}
